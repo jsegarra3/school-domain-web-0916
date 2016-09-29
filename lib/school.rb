be4e44703d250d2bb614553f31c1247f0b4f050b
James Segarra III <JamesSegarra@gmail.com>
2016-09-29 14:18:51 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(school,roster = {})
+    @school = school
+    @roster = roster
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student,year)
+    @roster[year]=[] unless @roster.has_key?(year)
+    @roster[year]<<student
+  end
+
+  def grade(year)
+    @roster[year] if @roster.has_key?(year)
+  end
+
+  def sort
+    @roster.keys.sort!
+    @roster.values.map! {|student_list| student_list.sort!}
+    @roster
+  end
+  # binding.pry
+end
